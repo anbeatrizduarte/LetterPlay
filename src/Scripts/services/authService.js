20fcfd772eafd419db913c9262d015e2efe53d5a
kaynan <78617015+kaynanxd@users.noreply.github.com>
2025-12-08 19:57:06 -0300

    Update authService.js

--- src/Scripts/services/authService.js
+++ src/Scripts/services/authService.js
@@ -1,17 +1,22 @@
 import api from './api';
 
 export const loginUser = async (username, password) => {
-    const dadosLogin = new FormData();
-    dadosLogin.append('username', username);
-    dadosLogin.apénd('password', password);
+
+    const params = new URLSearchParams();
+    params.append('username', username); 
+    params.append('password', password);
 
     try {
-        const response = await api.post ('/auth/token/', dadosLogin);
+
+        const response = await api.post('/auth/token', params);
+
         if (response.data.access_token) {
-            localStorage.setItem('@LetterPLay:token', response.data.access_token);
+
+            localStorage.setItem('@LetterPlay:token', response.data.access_token);
         }
+        
         return response.data;
-    }catch (error){
+    } catch (error) {
         console.error("Erro no Login", error);
         throw error;
     }
